a9319358e85c8d83ba64afd8a0d86ab2d1ca1b88
栗田優磨 <krt.ym08@gmail.com>
2019-01-24 20:48:15 +0900

    change_message_js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,9 @@
 $(function(){
   function buildHTML(message){
+    var image = '';
+    if (message.image.url) {
+      image = `<img src="${message.image.url}" class="lower-message__image">`;
+    }
     var html = `<div class="comment">
                   <div class="message-user">
                   ${message.user_name}
@@ -10,30 +14,33 @@ $(function(){
                   <p class="lower-message__content">
                   ${message.text}
                   </p>
+                  ${image}
                 </div>`
     return html;
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false,
     })
     .done(function(data){
       var html = buildHTML(data);
       $('.contents-right').append(html)
-      $('.text-form').val('')
+      $('#new_message')[0].reset();
       $('.contents-right').animate({scrollTop: $('.contents-right')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
       alert('error');
-    });
-    return false
+    })
+    setTimeout(function(){
+      $('.form__submit').prop('disabled', false)
+    }, 1000);
   });
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,3 +1,4 @@
 json.text  @message.content
 json.user_name  @message.user.name
 json.timestamp  @message.created_at
+json.image  @message.image
